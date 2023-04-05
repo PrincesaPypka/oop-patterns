84eda880aba4bba0c2d47339e1a1a4c71b8bda50
PrincesaPypka <nikolya.2002@list.ru>
2023-04-06 00:46:48 +0300

    Methods implementation split from class

--- observer-pattern/WeatherStation/weatherData/data.hpp
+++ observer-pattern/WeatherStation/weatherData/data.hpp
@@ -18,63 +18,25 @@ class WeatherData {
       pressure_(s.pressure_),
       connected_devises_(std::move(s.connected_devises_)) {}
 
-  void AddDevice(const std::shared_ptr<DeviceInterface> &device) {
-      connected_devises_.push_back(device);
-  }
+  void AddDisplay(const std::shared_ptr<DeviceInterface> &device);
 
-  void RemoveDevice(const std::shared_ptr<DeviceInterface> &device) {
-      auto ptr = device.get();
-      long pos = -1;
-      for (auto i = 0; i < connected_devises_.size(); ++i)
-          if (ptr == connected_devises_[i].get()) {
-              pos = i;
-              break;
-          }
-      if (pos != -1)
-          connected_devises_.erase(connected_devises_.cbegin() + pos);
-  }
+  void RemoveDisplay(const std::shared_ptr<DeviceInterface> &device);
 
-  void NotifyDevices() {
-      for (const auto &device : connected_devises_) device->Update();
-  }
+  void NotifyDisplay();
 
   [[nodiscard]] double GetTemperature() const { return temperature_; }
   [[nodiscard]] double GetHumidity() const { return humidity_; }
   [[nodiscard]] double GetPressure() const { return pressure_; }
 
-  void SetTemperature(double temperature) {
-      temperature_ = temperature;
-      NotifyDevices();
-  }
-  void SetHumidity(double humidity) {
-      humidity_ = humidity;
-      NotifyDevices();
-  }
-  void SetPressure(double pressure) {
-      pressure_ = pressure;
-      NotifyDevices();
-  }
+  void SetTemperature(double temperature);
+  void SetHumidity(double humidity);
+  void SetPressure(double pressure);
 
-  void SetAllData(double temperature, double humidity, double pressure) {
-      temperature_ = temperature;
-      humidity_ = humidity;
-      pressure_ = pressure;
-      NotifyDevices();
-  }
+  void SetAllData(double temperature, double humidity, double pressure);
 
-  WeatherData &operator=(const WeatherData &new_wd) {
-      temperature_ = new_wd.temperature_;
-      humidity_ = new_wd.humidity_;
-      pressure_ = new_wd.pressure_;
-      return *this;
-  }
+  WeatherData &operator=(const WeatherData &new_wd);
 
-  WeatherData &operator=(WeatherData &&new_wd) noexcept {
-      temperature_ = new_wd.temperature_;
-      humidity_ = new_wd.humidity_;
-      pressure_ = new_wd.pressure_;
-      return *this;
-  }
+  WeatherData &operator=(WeatherData &&new_wd) noexcept;
 
  private:
   double temperature_;
