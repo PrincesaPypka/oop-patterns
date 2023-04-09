bdd52f5ab5162780aef1a74b8dbbea8a4950bf27
PrincesaPypka <nikolya.2002@list.ru>
2023-04-09 21:07:02 +0300

    documentation added

--- decorator-pattern/StarbuzzCofe/beverage/interface.hpp
+++ decorator-pattern/StarbuzzCofe/beverage/interface.hpp
@@ -3,15 +3,35 @@
 
 #include <string>
 #include <utility>
+#include <iostream>
 
+/**
+  * @class BeverageInterface
+  * @brief The abstract base class for all beverage classes
+  * @details BeverageInterface provides a common interface for all the
+  * beverages that will be served.
+  * */
 class BeverageInterface {
  public:
+  /**
+    * @brief Get the description of the beverage
+    * @return const std::string& The description of the beverage
+    * */
   [[nodiscard]] virtual const std::string &GetDescription() const {
       return description_;
   }
+  /**
+    * @brief Get the cost of the beverage
+    * @return double The cost of the beverage
+    * */
   [[nodiscard]] virtual double Cost() const = 0;
+  /// @brief Virtual destructor
   virtual ~BeverageInterface() = 0;
  protected:
+  /**
+    * @brief Constructor for BeverageInterface class
+    * @param description The description of the beverage
+    * */
   explicit BeverageInterface(std::string description = "Unknown Beverage") :
       description_(std::move(description)) {}
   std::string description_;
--- decorator-pattern/StarbuzzCofe/condiment/decorator.hpp
+++ decorator-pattern/StarbuzzCofe/condiment/decorator.hpp
@@ -3,14 +3,32 @@
 
 #include "beverage/interface.hpp"
 
+/**
+  * @class CondimentDecorator
+  * @brief The abstract base class for all condiment decorator classes
+  * @details CondimentDecorator is an abstract base class that provides a
+  * common interface for all the condiment decorators that will be used to
+  * decorate a beverage.
+  * */
 class CondimentDecorator : public BeverageInterface {
  public:
-  explicit CondimentDecorator(BeverageInterface *beverage) :
-      beverage_(beverage) {}
+  /**
+    * @brief Get the cost of the decorated beverage
+    * @return double The cost of the decorated beverage
+    * */
   [[nodiscard]] double Cost() const override = 0;
-  virtual ~CondimentDecorator() = 0;
+  ~CondimentDecorator() override = 0;
 
  protected:
+  /**
+    * @brief Constructor for CondimentDecorator class
+    * @param beverage A pointer to the BeverageInterface object to be decorated
+    * */
+  explicit CondimentDecorator(BeverageInterface *beverage) :
+      beverage_(beverage) {
+      std::cout << "CondimentDecor " << this <<
+                " beverage " << beverage << std::endl;
+  }
   BeverageInterface *beverage_;
 };
 
